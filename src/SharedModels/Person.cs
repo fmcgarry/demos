9beda59fabf341469d5d7b74369acbdb08e154c1
Freddy McGarry <fmcgarry@minitab.com>
2022-01-31 14:23:27 -0500

    added shared Person Model

---
+++ src/SharedModels/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedModels
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
