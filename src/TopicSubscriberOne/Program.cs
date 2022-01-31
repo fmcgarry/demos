691985112254190f98fa2e658864faebd0db66b3
Freddy McGarry <fmcgarry@minitab.com>
2022-01-31 16:48:31 -0500

    TopicSubscriberOne now peeks all messages

--- src/TopicSubscriberOne/Program.cs
+++ src/TopicSubscriberOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using SharedModels;
 
 // Build a config object, using env vars and JSON providers.
 IConfiguration config = new ConfigurationBuilder()
@@ -10,3 +11,22 @@ IConfiguration config = new ConfigurationBuilder()
 // Get values from the config given their key and their target type.
 Settings settings = config.GetRequiredSection(nameof(Settings)).Get<Settings>();
 string connectionString = config.GetConnectionString("AzureServiceBus");
+
+var serviceBusFacade = new ServiceBusSubscriber(settings.TopicName, connectionString);
+
+var cancellationToken = new CancellationTokenSource();
+
+var people = await serviceBusFacade.PeekPeople(cancellationToken.Token);
+
+
+if (people is not null)
+{
+    foreach (var person in people)
+    {
+        Console.WriteLine(person);
+    }
+}
+else
+{
+    Console.WriteLine("No people found.");
+}
