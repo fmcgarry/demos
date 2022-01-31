e4b0d7ecc4657e40b6528506f3af159cd320cf53
Freddy McGarry <fmcgarry@minitab.com>
2022-01-31 13:29:49 -0500

    Added cosole program for topic publisher

--- src/TopicPublisher/Program.cs
+++ src/TopicPublisher/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+// Get values from the config given their key and their target type.
+Settings settings = config.GetRequiredSection(nameof(Settings)).Get<Settings>();
+
+Console.WriteLine(settings.MyProperty);
---
+++ src/TopicPublisher/Settings.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Settings
+{
+    public string MyProperty { get; set; }
+}
