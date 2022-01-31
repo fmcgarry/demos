879bb1f6d21cd0ebedc8493135eac276bf795529
Freddy McGarry <fmcgarry@minitab.com>
2022-01-31 13:44:30 -0500

    created project TopicSubscriberTwo

---
+++ src/TopicSubscriberTwo/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+// Get values from the config given their key and their target type.
+Settings settings = config.GetRequiredSection(nameof(Settings)).Get<Settings>();
