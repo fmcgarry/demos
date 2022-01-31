4a35d0f45f5428b20c6c4e789bc0accf82a34686
Freddy McGarry <fmcgarry@minitab.com>
2022-01-31 16:02:51 -0500

    added ToString to Person model

--- src/SharedModels/Person.cs
+++ src/SharedModels/Person.cs
@@ -4,5 +4,10 @@
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
     }
 }
