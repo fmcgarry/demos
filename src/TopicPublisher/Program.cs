8fba184272a7984fb2a5bd75628f601158c37c3f
Freddy McGarry <fmcgarry@minitab.com>
2022-01-31 16:06:30 -0500

    finished publish project

--- src/TopicPublisher/Program.cs
+++ src/TopicPublisher/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
+using SharedModels;
+using TopicPublisher;
 
 // Build a config object, using env vars and JSON providers.
 IConfiguration config = new ConfigurationBuilder()
@@ -10,3 +12,22 @@ IConfiguration config = new ConfigurationBuilder()
 // Get values from the config given their key and their target type.
 Settings settings = config.GetRequiredSection(nameof(Settings)).Get<Settings>();
 string connectionString = config.GetConnectionString("AzureServiceBus");
+
+var serviceBusFacade = new ServiceBusPublisher(connectionString, settings.TopicName);
+
+Console.WriteLine("Enter a person...");
+Console.Write("First Name: ");
+var firstName = Console.ReadLine();
+Console.Write("Last Name: ");
+var lastName = Console.ReadLine();
+
+bool isSuccess = await serviceBusFacade.SendPerson(new Person() { FirstName = firstName, LastName = lastName });
+
+if (isSuccess)
+{
+    Console.WriteLine("Success");
+}
+else
+{
+    Console.WriteLine("Something happened!");
+}
---
+++ src/TopicPublisher/ServiceBusPublisher.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Messaging.ServiceBus;
+using SharedModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopicPublisher
+{
+    internal class ServiceBusPublisher
+    {
+        private static ServiceBusClient _client;
+        private static ServiceBusSender _sender;
+
+        public ServiceBusPublisher(string connectionString, string topic)
+        {
+            _client = new ServiceBusClient(connectionString);
+            _sender = _client.CreateSender(topic);
+        }
+
+        public async Task<bool> SendPerson(Person person)
+        {
+            try
+            {
+                ServiceBusMessage message = CreateMessageFrom(person);
+                await _sender.SendMessageAsync(message);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to publish {person}: {e}");
+                return false;
+            }
+        }
+
+        private static ServiceBusMessage CreateMessageFrom(Person person)
+        {
+            var serializedPerson = System.Text.Json.JsonSerializer.Serialize(person);
+            var message = new ServiceBusMessage(body: serializedPerson);
+
+            return message;
+        }
+    }
+}
