1cbf44b2ff2892c1a6508ae7950b3e75da1646bf
Freddy McGarry <fmcgarry@minitab.com>
2022-09-30 15:21:30 -0400

    uploadeding work in progress

---
+++ src/DemoApp.Web/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace azure_cosmosdb_demo.Models
+{
+    public class Item
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; } = "";
+        public DateTime DueDate { get; set; } = DateTime.Now;
+        public bool IsDone { get; set; } = false;
+    }
+}
